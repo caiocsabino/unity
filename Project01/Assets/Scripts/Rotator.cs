0a8c03621338f28f51008bf5fe15cec4e770350b
Caio Sabino <caiocsabino@gmail.com>
2014-01-01 19:17:52 +0200

    added rotation to the windmill

---
+++ Project01/Assets/Scripts/Rotator.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour
+{
+	public float m_angleSpeed = 20.0f;
+	private bool m_rotationEnabled = false;
+
+	void Update ()
+	{
+		if (Input.GetKeyUp (KeyCode.Return)) 
+		{
+			m_rotationEnabled = !m_rotationEnabled;
+		}
+
+		if(m_rotationEnabled)
+			transform.Rotate(new Vector3(1,0,0), m_angleSpeed * Time.deltaTime);
+	}
+}

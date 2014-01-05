946b197bac4968d114e711f30e049156fb36ef1b
Caio Sabino <caiocsabino@gmail.com>
2014-01-05 20:29:48 +0200

    added animations to door

--- Project01/Assets/Scripts/Lever.cs
+++ Project01/Assets/Scripts/Lever.cs
@@ -8,6 +8,15 @@ public class Lever : MonoBehaviour {
 
 	private GameObject m_lever;
 
+	private float m_animationTimer = -1.0f;
+
+	private float m_currentAngle = 270.0f;
+
+	private float m_totalAnimationTime = 0.5f;
+
+	private float m_startAngle = 270;
+	private float m_endAngle = 360;
+
 	// Use this for initialization
 	void Start () 
 	{
@@ -22,7 +31,28 @@ public class Lever : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (m_animationTimer > -1) 
+		{
+			m_animationTimer += Time.deltaTime;
+			m_animationTimer = Mathf.Min(m_animationTimer, m_totalAnimationTime);
+
+			float ratio = (m_animationTimer/m_totalAnimationTime);
+			m_currentAngle =  m_startAngle + (m_endAngle - m_startAngle) * ratio;
+
+			GameObject actualLever = transform.FindChild("actualLever").gameObject;
+			
+			if(actualLever)
+			{
+				actualLever.transform.rotation = Quaternion.AngleAxis(m_currentAngle, new Vector3(1,0,0));
+			}
+
+			if(m_animationTimer == m_totalAnimationTime)
+			{
+				m_animationTimer = -1.0f;
+
+				m_rotatorComponent.ToggleRotation();
+			}
+		}
 	}
 
 	public void Activate()
@@ -30,13 +60,13 @@ public class Lever : MonoBehaviour {
 		Debug.Log ("lever activated");
 		if (m_rotatorComponent) 
 		{
-			m_rotatorComponent.ToggleRotation();
 
-			GameObject actualLever = transform.FindChild("actualLever").gameObject;
-
-			if(actualLever)
+			if (m_animationTimer < 0) 
 			{
-				actualLever.transform.Rotate (new Vector3 (0, 1, 0), -90);
+				m_animationTimer = 0;
+				m_startAngle = m_currentAngle;
+				m_endAngle = m_startAngle + 90;
+
 			}
 		}
 	}
---
+++ Project01/Assets/Scripts/Windmill.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Windmill : MonoBehaviour {
+
+	private GameObject m_door;
+	// Use this for initialization
+	void Start () {
+		m_door = transform.FindChild("prefab_windmill_door").gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag ("Player")) 
+		{
+			Debug.Log ("Trigger entered on windmill");
+
+			if(m_door)
+			{
+				//Animation openAnimation = m_door.GetComponent<Animation>();
+				//openAnimation.Play();
+				m_door.transform.animation.Play("anim_door_open");
+				//m_door.transform.Rotate(new Vector3(0,1,0), 90);
+			}
+		}
+	}
+	public void OnTriggerExit(Collider other)
+	{
+		if (other.CompareTag ("Player"))
+		{
+			Debug.Log ("Trigger exited on windmill");
+			m_door.transform.animation.Play("anim_door_close");
+			//m_door.transform.Rotate(new Vector3(0,1,0), -90);
+		}
+	}
+}
